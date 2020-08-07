1070290b103de24fc1d4c795b7a167c998000c8d
whr <whr@whr.local>
2020-08-07 11:57:15 +0800

    aaaa

---
+++ userref/ep/a_test.go
@@ -0,0 +1,13 @@
+package ep
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_aaa(t *testing.T) {
+	fmt.Println("abc")
+	_, err := new(InnerMasterInfoGetH).Call(InnerMasterInfoGetIn{})
+
+	fmt.Println(err)
+}
