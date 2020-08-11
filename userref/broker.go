d229d3998001aea387635b929c3120803da4d240
whr <whr@whr.local>
2020-08-11 10:43:47 +0800

    aaaa

---
+++ userref/broker.go
@@ -0,0 +1,36 @@
+package userref
+
+import (
+	"github.com/vhaoran/vchat/lib/ypg"
+)
+
+//代理商家
+type Broker struct {
+	ypg.BaseModel
+	ID int64
+	//主体名称
+	Name string `json:"name" gorm:"name:name;type:text;null;unique_index;"`
+	//主体简介
+	Brief string `json:"" gorm:"name:;type:text;null;"`
+	//头像路径
+	Icon string `json:"icon" gorm:"name:icon;type:text;null;"`
+
+	//拥有者id,来自于userInfo
+	OwnerID       int64
+	OwnerUserCode string `json:"owner_user_code" gorm:"name:owner_user_code;type:varchar(100);null;"`
+	OwnerNick     string `json:"owner_nick" gorm:"name:owner_nick;type:text;null;"`
+	OwnerIcon     string `json:"owner_icon" gorm:"name:owner_icon;type:text;null;"`
+
+	//提现帐号，只能来自微信或支付宝
+	//0 支付宝 1：微信
+	//只能是两者之一
+	AccountType int
+	AccountCode string `json:"account_code" gorm:"name:account_code;type:text;null;"`
+
+	//启用状态（0：停用 1：启用,默认启用）
+	Enabled int
+}
+
+func (Broker) TableName() string {
+	return "broker"
+}
