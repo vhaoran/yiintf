e57a015857aff5502f1b8791493c0a7e909ed198
whr <whr@whr.local>
2020-09-22 16:47:22 +0800

    aaaa

--- userref/ep/innerIsSysAdmin-EP.go
+++ userref/ep/innerIsSysAdmin-EP.go
@@ -19,49 +19,49 @@ import (
 // 用途：根据uid判断用户是否是管理员
 //---------------------------------------------
 const (
-	IsAdmin_H_PATH = "/IsAdmin"
+	InnerIsSysAdmin_H_PATH = "/InnerIsSysAdmin"
 )
 
 //获取用户所有好友
 type (
-	IsAdminService interface {
-		Exec(in *IsAdminIn) (*IsAdminOut, error)
+	InnerIsSysAdminService interface {
+		Exec(in *InnerIsSysAdminIn) (*InnerIsSysAdminOut, error)
 	}
 
 	//input data
-	IsAdminIn struct {
+	InnerIsSysAdminIn struct {
 		UID int64 `json:"is_admin"`
 	}
 
 	//output data
-	IsAdminOut struct {
+	InnerIsSysAdminOut struct {
 		bool
 	}
 
 	// handler implements
-	IsAdminH struct {
+	InnerIsSysAdminH struct {
 		base ykit.RootTran
 	}
 )
 
-func (r *IsAdminH) MakeLocalEndpoint(svc IsAdminService) endpoint.Endpoint {
+func (r *InnerIsSysAdminH) MakeLocalEndpoint(svc InnerIsSysAdminService) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
 		fmt.Println("#############  GetUserFriendsInner ###########")
 		spew.Dump(ctx)
 
-		in := request.(*IsAdminIn)
+		in := request.(*InnerIsSysAdminIn)
 		return svc.Exec(in)
 	}
 }
 
 //个人实现,参数不能修改
-func (r *IsAdminH) DecodeRequest(ctx context.Context, req *http.Request) (interface{}, error) {
-	return r.base.DecodeRequest(new(IsAdminIn), ctx, req)
+func (r *InnerIsSysAdminH) DecodeRequest(ctx context.Context, req *http.Request) (interface{}, error) {
+	return r.base.DecodeRequest(new(InnerIsSysAdminIn), ctx, req)
 }
 
 //个人实现,参数不能修改
-func (r *IsAdminH) DecodeResponse(_ context.Context, res *http.Response) (interface{}, error) {
-	var response *IsAdminOut
+func (r *InnerIsSysAdminH) DecodeResponse(_ context.Context, res *http.Response) (interface{}, error) {
+	var response *InnerIsSysAdminOut
 	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
 		return nil, err
 	}
@@ -69,7 +69,7 @@ func (r *IsAdminH) DecodeResponse(_ context.Context, res *http.Response) (interf
 }
 
 //handler for router，微服务本地接口，
-func (r *IsAdminH) HandlerLocal(service IsAdminService,
+func (r *InnerIsSysAdminH) HandlerLocal(service InnerIsSysAdminService,
 	mid []endpoint.Middleware,
 	options ...tran.ServerOption) *tran.Server {
 
@@ -88,39 +88,39 @@ func (r *IsAdminH) HandlerLocal(service IsAdminService,
 }
 
 //sd,proxy实现,用于etcd自动服务发现时的handler
-func (r *IsAdminH) HandlerSD(mid []endpoint.Middleware,
+func (r *InnerIsSysAdminH) HandlerSD(mid []endpoint.Middleware,
 	options ...tran.ServerOption) *tran.Server {
 	return r.base.HandlerSD(
 		context.Background(),
 		msTag,
 		"POST",
-		IsAdmin_H_PATH,
+		InnerIsSysAdmin_H_PATH,
 		r.DecodeRequest,
 		r.DecodeResponse,
 		mid,
 		options...)
 }
 
-func (r *IsAdminH) ProxySD() endpoint.Endpoint {
+func (r *InnerIsSysAdminH) ProxySD() endpoint.Endpoint {
 	return r.base.ProxyEndpointSD(
 		context.Background(),
 		msTag,
 		"POST",
-		IsAdmin_H_PATH,
+		InnerIsSysAdmin_H_PATH,
 		r.DecodeRequest,
 		r.DecodeResponse)
 }
 
 //只用于内部调用 ，不从风头调用
-var once_IsAdmin sync.Once
-var local_IsAdmin_EP endpoint.Endpoint
+var once_InnerIsSysAdmin sync.Once
+var local_InnerIsSysAdmin_EP endpoint.Endpoint
 
-func (r *IsAdminH) Call(in *IsAdminIn) (*IsAdminOut, error) {
-	once_IsAdmin.Do(func() {
-		local_IsAdmin_EP = new(IsAdminH).ProxySD()
+func (r *InnerIsSysAdminH) Call(in *InnerIsSysAdminIn) (*InnerIsSysAdminOut, error) {
+	once_InnerIsSysAdmin.Do(func() {
+		local_InnerIsSysAdmin_EP = new(InnerIsSysAdminH).ProxySD()
 	})
 	//
-	ep := local_IsAdmin_EP
+	ep := local_InnerIsSysAdmin_EP
 	//
 	result, err := ep(context.Background(), in)
 
@@ -128,5 +128,5 @@ func (r *IsAdminH) Call(in *IsAdminIn) (*IsAdminOut, error) {
 		return nil, err
 	}
 
-	return result.(*IsAdminOut), nil
+	return result.(*InnerIsSysAdminOut), nil
 }
