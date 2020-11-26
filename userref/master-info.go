cb4ca44cb0e6d4e9b676538ebd2ff58725e8ee5e
whr <whr@whr.local>
2020-11-26 16:30:36 +0800

    aaaa

--- userref/master-info.go
+++ userref/master-info.go
@@ -11,8 +11,8 @@ import (
 //---------------------------------------------
 //大师基本信息
 type MasterInfoRef struct {
-	UID      int64  `json:"uid"`
-	UserCode string `json:"user_code"`
+	UID      int64  `json:"uid" gorm:"unique_index:uid_multi;"`
+	UserCode string `json:"user_code" gorm:"name:user_code;type:varchar(100);null;"`
 	Nick     string `json:"nick" gorm:"name:nick;type:text;null;"`
 	Icon     string `json:"icon" gorm:"name:icon;type:text;null;"`
 
@@ -21,12 +21,12 @@ type MasterInfoRef struct {
 	//返点比例，值为30表示30%
 	Rebate float64 `json:"rebate" gorm:"name:rebate;null;"`
 
-	Rate       int64      `json:"rate"`
-	SignDate   ytime.Date `json:"sign_date"`
-	BestRate   int64      `json:"best_rate"`
-	OrderTotal int64      `json:"order_total"`
-	MidRate    int64      `json:"mid_rate"`
-	BadRate    int64      `json:"bad_rate"`
-	Level      int64      `json:"level"`
-	Enabled    int        `json:"enabled"`
+	Rate       int64      `json:"rate" gorm:"name:rate;"`
+	SignDate   ytime.Date `json:"sign_date" gorm:"name:sign_date;"`
+	BestRate   int64      `json:"best_rate" gorm:"name:best_rate;"`
+	OrderTotal int64      `json:"order_total" gorm:"name:order_total;"`
+	MidRate    int64      `json:"mid_rate" gorm:"name:mid_rate;"`
+	BadRate    int64      `json:"bad_rate" gorm:"name:bad_rate;"`
+	Level      int64      `json:"level" gorm:"name:level;"`
+	Enabled    int        `json:"enabled" gorm:"name:enabled;"`
 }
