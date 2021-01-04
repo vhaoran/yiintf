7915f43480338108bf2819e87b40cb21768ad8ff
whr <whr@whr.local>
2021-01-04 14:07:40 +0800

    aaaa

--- userref/master-info.go
+++ userref/master-info.go
@@ -21,12 +21,19 @@ type MasterInfoRef struct {
 	//返点比例，值为30表示30%
 	Rebate float64 `json:"rebate" gorm:"name:rebate;null;"`
 
-	Rate       int64      `json:"rate" gorm:"name:rate;"`
-	SignDate   ytime.Date `json:"sign_date" gorm:"name:sign_date;"`
-	BestRate   int64      `json:"best_rate" gorm:"name:best_rate;"`
-	OrderTotal int64      `json:"order_total" gorm:"name:order_total;"`
-	MidRate    int64      `json:"mid_rate" gorm:"name:mid_rate;"`
-	BadRate    int64      `json:"bad_rate" gorm:"name:bad_rate;"`
-	Level      int64      `json:"level" gorm:"name:level;"`
-	Enabled    int        `json:"enabled" gorm:"name:enabled;"`
+	//评分
+	Rate     int64      `json:"rate" gorm:"name:rate;"`
+	SignDate ytime.Date `json:"sign_date" gorm:"name:sign_date;"`
+	//好评
+	BestRate int64 `json:"best_rate" gorm:"name:best_rate;"`
+	//总单数
+	OrderTotal int64 `json:"order_total" gorm:"name:order_total;"`
+	//中评
+	MidRate int64 `json:"mid_rate" gorm:"name:mid_rate;"`
+	//差评
+	BadRate int64 `json:"bad_rate" gorm:"name:bad_rate;"`
+	//评级
+	Level int64 `json:"level" gorm:"name:level;"`
+	//启用、停用状态
+	Enabled int `json:"enabled" gorm:"name:enabled;"`
 }
