6a06d0ce05a12bddcaad51faaeb1be373b18b40e
whr <whr@whr.local>
2021-01-13 16:07:17 +0800

    aaaa

---
+++ userref/rabbit/MasterExp.go
@@ -0,0 +1,11 @@
+package rabbit
+
+//mq queue
+const QUEUE_MASTER_EXP = "/yi/queue_master_exp"
+
+type MasterExpMq struct {
+	//大师id(uid)
+	MasterID int64 `json:"uid"`
+	// 评价结果 1：好 2 ：中 3：差
+	ExpResult int `json:"exp_result"`
+}
