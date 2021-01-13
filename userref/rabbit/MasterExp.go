299026fd7657675c565037ccc3911f41895762b0
whr <whr@whr.local>
2021-01-13 16:18:33 +0800

    aaaa

--- userref/rabbit/MasterExp.go
+++ userref/rabbit/MasterExp.go
@@ -5,7 +5,7 @@ const QUEUE_MASTER_EXP = "/yi/queue_master_exp"
 
 type MasterExpMq struct {
 	//大师id(uid)
-	MasterID int64 `json:"uid"`
+	MasterID int64 `json:"master_id"`
 	// 评价结果 1：好 2 ：中 3：差
 	ExpResult int `json:"exp_result"`
 }
